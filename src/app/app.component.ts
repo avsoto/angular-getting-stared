0f48e91db0fc0bb578381f61bb9b1dd626d2dfe0
avsoto <avsotomejia@gmail.com>
2024-12-18 19:32:52 -0500

    learning ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-getting-stared';
+
+  users:string[] = ['Pepe', 'Maria', 'Jose'];
 }
